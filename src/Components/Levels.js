d90184a3574ec5611166523c1ac69299b03b171f
Deblina Talukdar <deblina7talukdar@gmail.com>
2020-06-07 09:34:17 +0530

    Adding new levels with other changes

---
+++ src/Components/Levels.js
@@ -0,0 +1,19 @@
+import React, {Component, PropTypes} from 'react';
+import '../css/main.css';
+
+export default class Levels extends Component{
+	render(){
+		return(
+
+			<div className="main-page">
+				This is a mathematics game. There are <b>two levels</b> <br /><hr />
+				1. <b>Level 1 (Easy)</b> - This is for the newbie.<br/>
+				2. <b>Level 2 (Hard)</b> - This is not for the faint-hearted. Hahaha!! <br /><br/>
+				Since you are familiar with the rules, so let's get started without any more jibber-jabber <br /><br/>
+
+				<b>Let's see which level fascinates you !! :P </b>< br /><br/>
+			</div>
+
+		);
+	}
+}
